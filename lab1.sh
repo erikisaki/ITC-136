161bd8015f93beb9d690020ee70b04b44858064f
Yuxuan <myx880912@gmail.com>
2017-04-04 14:49:15 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,7 @@
 # This is a comment
+
+whoami            # Tells me which user I'm logging in (WHO)
+pwd               # Stands for 'Print working Directory'and tells me where I am (WHERE)
+ls                # Tells me what files are in the directory (WHAT)
+cd                # Allow me to change into other dirctory (NAVIGATION)
+date              # What time is it on the machine (WHEN)
