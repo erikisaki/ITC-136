eef8d1699a32cd9a4f586935a27a9df7f10790e8
Yuxuan <myx880912@gmail.com>
2017-04-05 18:57:33 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -5,3 +5,11 @@ pwd               # Stands for 'Print working Directory'and tells me where I am 
 ls                # Tells me what files are in the directory (WHAT)
 cd                # Allow me to change into other dirctory (NAVIGATION)
 date              # What time is it on the machine (WHEN)
+
+/var # sh test.sh  
+test.sh: line 1: !#/bin/sh: not found                                           
+This is a comment                                                               
+/var # cat test.sh                                                              
+!#/bin/sh                                                                       
+                                                                                
+echo "This is a comment" 
